8d1688a1494aab95a23ce5e33c28e717ba3abfd9
안하현 <54823186+anhahyoun@users.noreply.github.com>
2022-04-02 00:20:09 +0900

    5397 키로거 (스택, iterator)

--- kotlin/backjoon/5397.kt
+++ kotlin/backjoon/5397.kt
@@ -1,4 +1,34 @@
+import java.util.*
+
 fun main() {
+    stack()
+    iterator()
+}
+
+private fun iterator() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    repeat(br.readLine().toInt()) {
+        val list = LinkedList<Char>()
+        val iterator = list.listIterator()
+        br.readLine().trim().forEach { c ->
+            when (c) {
+                '<' -> if (iterator.hasPrevious()) iterator.previous()
+                '>' -> if (iterator.hasNext()) iterator.next()
+                '-' -> if (iterator.hasPrevious()) {
+                    iterator.previous()
+                    iterator.remove()
+                }
+                else -> iterator.add(c)
+            }
+        }
+        bw.write("${list.joinToString("")}\n")
+    }
+    bw.close()
+}
+
+private fun stack() {
     val br = System.`in`.bufferedReader()
     val bw = System.out.bufferedWriter()
 
